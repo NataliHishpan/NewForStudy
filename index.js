3339cf0eb37c9bd849b337d14250482e6a518cd0
NataliHishpan <107191232+NataliHishpan@users.noreply.github.com>
2022-06-09 14:26:57 +0200

    innert files

---
+++ index.js
@@ -0,0 +1,29 @@
+let country = "Guinea Bissau";
+country.replace(" ", "-");
+let city = " Sydney   ";
+let place = "School";
+let attraction = "Opera House";
+
+let now = new Date();
+console.log(now;
+
+let milliseconds = now.getMilliseconds();
+console.log(milliseconds);
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[now.getDay()];
+console.log(day);
+
+let year = now.getFullYear();
+console.log(year);
+
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let month = months[now.getMonth()];
+console.log(month);
+
+let date = now.getDate;
+
+console.log("Today is "+ day +", "+ month +" " + date + ", " + year);
+
+h2.innerHTML = ("Today is " + day + ", " + month + " " + date + ", " + year);
+
